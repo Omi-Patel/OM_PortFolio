e37e0b5af4bc8115574480ba8cd59da68a8f07f8
Om Patel <omupatel7113@gmail.com>
2024-05-30 16:16:39 +0530

    fixes home page

--- src/app/(home)/components/SpotlightPreview.tsx
+++ src/app/(home)/components/SpotlightPreview.tsx
@@ -20,9 +20,9 @@ export function SpotlightPreview() {
       />
 
       {/* inside  */}
-      <div className="h-[40rem] w-full rounded-md flex md:items-center md:justify-center antialiased bg-grid-white/[0.02] relative overflow-hidden ">
+      <div className="h-[50rem] w-full rounded-md flex md:items-center md:justify-center antialiased bg-grid-white/[0.02] relative overflow-hidden ">
         <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0 ">
-          <h3 className="mt-16 sm:mt-0 text-center font-medium text-xl tracking-wider">
+          <h3 className="mt-32 sm:mt-0 text-center font-medium text-xl tracking-wider">
             Hello, Developers..!
           </h3>
           <h1 className="mt-6 text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-r from-neutral-50 to-green-400/40 bg-opacity-50 ">
@@ -32,15 +32,17 @@ export function SpotlightPreview() {
             </span>{" "}
             <br />
           </h1>
-          <div className="mt-6 text-center sm:p-1 p-3  tracking-wider  mx-auto font-normal text-neutral-600 dark:text-neutral-400">
-            <FlipWords words={words} /> <br />
-          </div>
-          <div className="mt-10 tracking-wider  text-neutral-300/80 max-w-3xl text-center mx-auto capitalize ">
+          
+          <div className="mt-10 sm:mt-10 tracking-wider  text-neutral-300/80 max-w-3xl text-center mx-auto capitalize ">
             <TextGenerateEffect words={intro} />
           </div>
 
+          <div className="mt-10 text-center sm:p-1   sm:mt-8 tracking-wider  mx-auto font-normal text-neutral-600 dark:text-neutral-400">
+            <FlipWords words={words} /> <br />
+          </div>
+
           {/* know more button  */}
-          <div className="flex justify-center items-center mt-16">
+          <div className="flex justify-center items-center mt-14">
             <Link href={"/about"}>
               <Button
                 borderRadius="1.75rem"
